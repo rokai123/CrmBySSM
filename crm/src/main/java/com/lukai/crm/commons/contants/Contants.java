25159cbb4db9f9bd35594c345e12bdb7fe5c02e1
千鸟 <@DESKTOP-IR8TM06>
2025-10-25 16:53:00 +0900

    asdas

--- crm/src/main/java/com/lukai/crm/commons/contants/Contants.java
+++ crm/src/main/java/com/lukai/crm/commons/contants/Contants.java
@@ -1,7 +1,7 @@
 package com.lukai.crm.commons.contants;
 
 public class Contants {
-	//ReturnObjectクラスのCode値を保存する。sda打算
+	//ReturnObjectクラスのCode値を保存する。sda打算阿萨德
 	public static final String RETURN_OBJECT_CODE_SUCCESS="1";//成功
 	public static final String RETURN_OBJECT_CODE_FAIL="2";//失敗
 	
