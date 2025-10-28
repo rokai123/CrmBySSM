9c55cd7a196326761ad5b810668fe46f742cbb25
rokai123 <lu8508598@gmail.com>
2025-10-28 10:31:44 +0900

    1

--- crm/src/main/java/com/lukai/crm/commons/contants/Contants.java
+++ crm/src/main/java/com/lukai/crm/commons/contants/Contants.java
@@ -2,7 +2,7 @@ package com.lukai.crm.commons.contants;
 
 public class Contants {
 
-	//ReturnObjectクラスのCode値を保存する。我要合并到master分支里面了哦！
+	//ReturnObjectクラスのCode値を保存する。
 	public static final String RETURN_OBJECT_CODE_SUCCESS="1";//成功
 	public static final String RETURN_OBJECT_CODE_FAIL="2";//失敗
 	
